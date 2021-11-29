2c09f5cf6bf71eb0bf157cd57fe5c2189891892e
OlgaZtv <zatulivetrova@gmail.com>
2021-11-29 17:05:04 +0300

    added OwnerTests

--- src/test/java/guru/qa/tests/properties/OwnerTests.java
+++ src/test/java/guru/qa/tests/properties/OwnerTests.java
@@ -2,10 +2,12 @@ package guru.qa.tests.properties;
 
 import guru.qa.config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
 
+@Tag("properties")
 public class OwnerTests {
     public static CredentialsConfig credentials =
             ConfigFactory.create(CredentialsConfig.class);
